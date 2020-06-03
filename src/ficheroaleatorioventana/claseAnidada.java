cb85937487bcb4b5a1e67a2a4f12caa718053994
Samuel González <samuelglezglez@gmail.com>
2020-06-03 19:28:46 +0100

    Prueba GIT/GITHUB

---
+++ src/ficheroaleatorioventana/claseAnidada.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficheroaleatorioventana;
+
+/**
+ *
+ * @author Samuel
+ */
+class claseAnidada {
+    
+    void entrada() {
+        System.out.println("Método entrada.");
+    }
+
+    String salida(int d) {
+        System.out.println("Salida.");
+        return "Salida el " + d;
+    }
+    
+}
